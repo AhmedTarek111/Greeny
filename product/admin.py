c95c4eaf0f8cf26602983d0f66815d084c51ad0b
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-08-29 17:35:00 +0300

    customize admin

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Product,ProductImages,Brand,Review
 # Register your models here.
-admin.site.register(Product)
+class ImageProductInline(admin.TabularInline):
+    model = ProductImages
+class  ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','subtitle','image','price','flag','brand','description']
+    list_filter = ['name','subtitle','brand','description']
+    search_fields = ['name','subtitle','description']
+    inlines =[ImageProductInline,]
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImages)
 admin.site.register(Brand)
 admin.site.register(Review)
--- product/models.py
+++ product/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+
 class Product(models.Model):
-    FLAG_CHOCICES=(
+    FLAG_CHOICES=(
         ('Sale','Sale'),
         ('New','New'),
         ('Feature','Feature'),
@@ -16,14 +17,14 @@ class Product(models.Model):
     price = models.FloatField(verbose_name=_('Price'))
     sku = models.CharField(verbose_name=_('SKU'),max_length=150)
     description = models.TextField(verbose_name=_('Description'),max_length=40000)
-    flag = models.CharField(verbose_name=_('Flag'),max_length=50,choices=FLAG_CHOCICES)
+    flag = models.CharField(verbose_name=_('Flag'),max_length=50,choices=FLAG_CHOICES)
     brand = models.ForeignKey('Brand',verbose_name=_('Brand'),on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return self.name
 
 class ProductImages(models.Model):
+    product= models.ForeignKey(Product,verbose_name=_("Proudct"),on_delete=models.CASCADE)
     image = models.ImageField(verbose_name=_("Image"),upload_to='proudct_images')
-    proudct= models.ForeignKey(Product,verbose_name=_("Proudct"),on_delete=models.CASCADE)
     def __str__(self):
         return str(self.proudct)
     
@@ -36,9 +37,9 @@ class Brand(models.Model):
 
 class Review(models.Model):
     user = models.ForeignKey(User,verbose_name=_("User"),on_delete=models.SET_NULL,null=True)
-    proudct = models.ForeignKey(Product,verbose_name=_("Proudct"),on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,verbose_name=_("Product"),on_delete=models.CASCADE)
     review = models.TextField(verbose_name=_("Review"),max_length=500)
     rate = models.IntegerField(verbose_name=_("Rate"),)
     date = models.DateTimeField(verbose_name=_("Date"),default=timezone.now)
     def __str__(self):
-        return str(self.proudct)
+        return f"{str(self.product)} -> {self.review}"
