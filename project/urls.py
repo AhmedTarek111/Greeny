af9d6794758ef2ee1e3874b4fc79c2ef45a94109
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-09-17 22:02:55 +0300

    add home page

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
+    path('', include('settings.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 
 ]
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from product.models import Product,Brand,Review
+from django.db.models import Count,Avg
+from django.db.models.functions import Round  
+from django.db.models.functions import Coalesce
+def home(request):
+    
+    context={
+        'brands':Brand.objects.distinct()[:10].annotate(brands_count=Count('brand_product')),
+        'sold_products': Product.objects.filter(flag='Sale')[:10].annotate(a_r=Round(Avg('product_review__rate'),1)),
+        'feature_products': Product.objects.filter(flag='Feature')[:6].annotate(a_r=Round(Coalesce('product_review__rate',0),1)),
+        'new_products': Product.objects.filter(flag='New')[:10].annotate(a_r=Round(Coalesce('product_review__rate',0),1)),
+        'reviews':Review.objects.all()[:4]
+    }
+    return render( request,'settings/home.html',context)
