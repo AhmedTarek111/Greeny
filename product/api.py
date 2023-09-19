05e01cc07ea086319dd0371c1db0fa11263aa026
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-09-19 22:23:31 +0300

    order API

--- product/api.py
+++ product/api.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import filters
 from .serializers import *
 from .models import Product ,Brand
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,8 +11,9 @@ from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,Ret
 class product_list_API(ListAPIView):
     queryset = Product.objects.all()    
     serializer_class = ProductListSerializers
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'subtitle','description',]
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter]
+    filterset_fields = ['name',]
+    ordering_fields = ['name', 'price']
 class CreateUpdateDelete(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()   
     serializer_class = ProductDetailSerializers
