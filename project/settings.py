02dba7c236c991ddcb168bce0a64ba4e602dd2cd
and <ahmedtarekalsaudi@gmail.com>
2024-03-19 22:45:43 +0200

    add the passwords authentiation pages

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'orders',
     'redis',
+    "bootstrap5",
 ]
 
 SITE_ID = 1
@@ -198,3 +199,6 @@ AUTHENTICATION_BACKENDS = [
 
 # CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 # CELERY_RESULT_BACKEND="redis://127.0.0.1:6379/0"
+
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
